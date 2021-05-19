83d0942fa3e0b1a2e3cc171dde46687d339780bc
sagor ahammed <81686528+sagor20202021@users.noreply.github.com>
2021-05-19 16:07:17 +0600

    added html or css file

---
+++ index1.js
@@ -0,0 +1,110 @@
+
+
+// for(var i=0;i<=15;i++){
+//   if(i===0){
+//     console.log(i  +  " is even");
+//   }else if(i%2===0){
+//     console.log(i +  " is even");
+//   }else{
+//     console.log(i+ " is odd");
+//   }
+// }
+
+
+
+
+// var students =[['David', 80,20,40], ['Vinoth', 77,30,20], ['Divya', 88,50,500], ['Ishitha', 95,100,200], ['Thomas', 68,200,100]];
+
+// var sagor = 0;
+
+// for (var i=0; i < students.length; i++) {
+//       //   Avgmarks = Avgmarks + students[i][1];
+//       //  console.log(Avgmarks);
+//       sagor =students[i][3];
+//       console.log(sagor);
+// }
+
+// var students =[['david',20,50],['vinoth',30,54],['sagor',60,100],['naima altaf',65,98]];
+//    sagor =students.filter(n=> typeof i!==String);
+//   console.log(sagor);
+
+// var avrg=0;
+// for (var i=0;i<students.length;i++){
+//  nul=students[i];
+//   console.log(nul);
+//   avrg=avrg+students[i][1];
+//   console.log(avrg)
+// }
+
+
+
+
+// var name= "sagor ahammed";
+// var age = 18;
+// var email = "sagor562@gamil.com";
+// var country = "Bangladesh";
+// var pass = "sagor123";
+// var confirmpass = "sagor123";
+
+// if(age < 18){
+// }else if(age => 18){
+//   console.log("Your regestration succesfull");
+// }
+//  if(pass != confirmpass){
+//   console.log("Your password is wrong");
+// }else{
+//   console.log("Your password is write");
+// }
+
+// if(country!= 'Bangladesh'){
+//   console.log("other country not alloed (only for Bangladesh)");
+// }else{
+//   console.log("welcome our page ");
+// }
+
+// var dogs = {
+//   Fido: "Mutt",  Hunter: "Doberman",  Snoopie: "Beagle"
+// };
+// var myDog = "Hunter";
+// var myBreed = dogs[myDog];
+// console.log(myBreed);
+
+
+// var sagor={
+//   12:"Messi",
+//   13: "Ronaldo",
+//   15: "Neimar",
+//   16: "sagor"
+// };
+// var playerName = 12;
+// var player = sagor[playerName];
+// console.log(player);
+
+// var kamal=16;
+// var numberOf=sagor[kamal];
+// console.log(numberOf);
+
+// var ourDog = {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"]
+// };
+// var NameAmar= "name";
+// var sagor= ourDog[NameAmar];
+// console.log(sagor);
+// sagor1= ourDog.name="Happy Camper";
+// console.log(sagor1);
+
+// naima = ourDog["name"] = "Happy Camper sagor";
+// console.log(naima);
+
+// console.log(naima + "  and  "+ sagor1);
+
+
+// var sagor=[10,20,"sagor1","naima",60,70];
+// var jala = sagor.filter(function(number){ 
+//   return number==="number";
+ 
+// });
+// console.log(jala);
